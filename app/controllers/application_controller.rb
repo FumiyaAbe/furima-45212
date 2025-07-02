369409f21dc7040298ff6111f27ffa04fa6f9442
fumiya <f40ya246@icloud.com>
2025-07-02 20:34:54 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  # 全ページでBasic認証を有効にする
+  before_action :basic_auth
+
+  # ルートパス用のindexアクション
   def index
     render template: 'items/index'
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
